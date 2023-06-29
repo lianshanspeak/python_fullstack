c7b5b5171db154fb0fda2574d1b5cf5128100b14
ly201552y@163.com <ly201552y@163.com>
2023-06-29 23:13:16 +0800

    完成logo,header部分

---
+++ test_page/js/js1.js
@@ -0,0 +1,20 @@
+var flag=true;
+
+function show_menu() {
+        var menu1=document.getElementById("menu1");
+        if(flag){
+                menu1.style.display="block";
+                flag=false;
+        }else {
+                menu1.style.display="none";
+                flag=true;
+        }
+
+}
+
+function show_menu1(){
+        var menu1=document.getElementById("menu1");
+        menu1.style.display="none";
+        //理解为什么要写下面这一个
+        flag=true;
+}
