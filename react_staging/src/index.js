2f41bc7a54e7bf29ed878b519f1135f64deed8cb
lianshanspeak <ly201552y@163.com>
2023-07-26 23:56:32 +0800

    创建了gitignore文件，不同步某些文件

--- react_staging/src/index.js
+++ react_staging/src/index.js
@@ -1,10 +1,18 @@
 // 引入核心库
 import React from 'react';
 // 引入ReactDOM
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 // 引入App组件
 import App from './App';
 
 
 // 渲染到App页面
-ReactDOM.render(<App/>,document.getElementById('root'))
+// ReactDOM.render(<App/>,document.getElementById('root'))
+
+
+
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render( <App/> );
